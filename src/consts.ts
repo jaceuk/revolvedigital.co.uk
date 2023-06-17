6da29dcc354ef26010cc046510dab81f99c74f56
jaceuk <jnewington@gmail.com>
2023-06-17 23:34:45 +0100

    Build

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,2 @@
 export const SITE_TITLE = 'My personal website.';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
-
-// Homepage
-export const HOME_TITLE = 'Cost effective business websites';
-export const HOME_SUB_TITLE =
-	'With over two decades of experience let us help your business reach new heights with a bespoke website';
-export const HERO_ACTION_TEXT = 'Talk to us today';
-
-// Footer
-export const COPYRIGHT_TEXT = 'Revolve Digital Solutions. All rights reserved.';
