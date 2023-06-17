d707f2b16ef3de61c7a193878245d89b50ed3092
jaceuk <jnewington@gmail.com>
2023-06-17 12:46:45 +0100

    Build

---
+++ src/consts.ts
@@ -0,0 +1,8 @@
+// Homepage content
+export const homeTitle = 'Cost effective business websites';
+export const homeSubTitle =
+	'With over two decades of experience let us help your business reach new heights with a bespoke website';
+export const heroActionText = 'Talk to us today';
+
+// Footer content
+export const copyrightText = 'Revolve Digital Solutions. All rights reserved.';
